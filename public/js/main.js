156c2301dc60d4932f162efc6a3a33f7d3d8b8e4
danfaeh <danfaeh@gmail.com>
2017-02-27 10:28:50 -0800

    added prevent default to auth form

--- public/js/main.js
+++ public/js/main.js
@@ -8,26 +8,23 @@ $(function(){
   $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
   $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 
-  $('#admin').click(function(){
+  $('#admin').click(function(e){
+    e.preventDefault();
     var pass = $('#password').val();
-    if(pass === "dunedin"){
-      document.cookie = pass;
-      window.location.href = "/";
-    }
-    // $.ajax({
-    //   type: "POST",
-    //   url: "/auth",
-    //   data: {"pass": pass},
-    //   success: function(data) { 
-    //     if(data){
-    //       document.cookie = pass;
-    //       window.location.href = "/";
-    //       console.log("dunedin rocks!");
-    //     } else {
-    //       $('#error').show();
-    //     }  
-    //   }
-    // });    
+    $.ajax({
+      type: "POST",
+      url: "/auth",
+      data: {"pass": pass},
+      success: function(data) { 
+        if(data){
+          document.cookie = pass;
+          window.location.href = "/";
+          console.log("dunedin rocks!");
+        } else {
+          $('#error').show();
+        }  
+      }
+    });    
   });
 
 
