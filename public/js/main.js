fde9c6c6693ea5ff0e10144b187a7371a3795d35
danfaeh <danfaeh@gmail.com>
2017-03-22 07:46:16 -0700

    cart changes using local storage and jquery

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ console.log('working');
 $('#error').hide();
 $('#itemAddText').hide();
 
+//*******************************************************//
+//                    Cart Updates                       //
+//*******************************************************//
 
 function test123(){
   alert(">>>>Paypal Checkout Integration Goes Here<<<<");
@@ -13,16 +16,22 @@ function removeCartItem(item){
   window.location.reload(false); 
 }
 
-function addToCart(id,name){
+function addToCart(id,name,price){
   $('#itemAddText').show();
 
   itemQuantity = $('#itemQuantity').val();
-  var cartItem = { 'id': id, 'name':name, 'quantity': itemQuantity};
+  var total = (itemQuantity*price).toFixed(2);
+  var cartItem = { 'id': id, 'name':name, 'quantity': itemQuantity, 'price':price, 'total':total};
   // Put the object into storage
   localStorage.setItem(id, JSON.stringify(cartItem));
 }   
 
-//Once page Loads
+//*******************************************************//
+//*******************************************************//
+//*******************************************************//
+
+
+//On Page Load
 $(function(){ 
 
   // $('#headlineTop').arctext({radius: 300});
@@ -50,16 +59,23 @@ $(function(){
     });    
   });
 
-// Populate Shopping Cart Using Local Storage
+// Populate Shopping Cart Items and Totals using Local Storage
 if(window.location.href.indexOf("cart") > -1){
-  cartItems = window.localStorage;
-
+  var cartItems = window.localStorage;
+  var subtotal = 0;
+  var shipping = 10;
   for (var item in cartItems){
     itemObj = JSON.parse(window.localStorage.getItem(item));
-    cartItemString = '<tr><td class="col-sm-8 col-md-6"><div class="media"><a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a><div class="media-body"><h4 class="media-heading"><a href="#">'+itemObj.name+'</a></h4><h5 class="media-heading"> by <a href="#">Lyndee Lyndsey</a></h5><span>Status: </span><span class="text-success"><strong>In Stock</strong></span></div></div></td><td class="col-sm-1 col-md-1" style="text-align: center"><input type="number" class="form-control" id="quantity" value="'+itemObj.quantity+'"></td><td class="col-sm-1 col-md-1 text-center"><strong>$21.99</strong></td><td class="col-sm-1 col-md-1 text-center"><strong>$21.99</strong></td><td class="col-sm-1 col-md-1"><button type="button" class="btn btn-danger" onclick="removeCartItem(\''+itemObj.id+'\')"><span class="glyphicon glyphicon-remove"></span> Remove</button></td></tr>';
+    subtotal = subtotal + (itemObj.price * itemObj.quantity);
+    cartItemString = '<tr><td class="col-sm-8 col-md-6"><div class="media"><a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a><div class="media-body"><h4 class="media-heading"><a href="#">'+itemObj.name+'</a></h4><h5 class="media-heading"> by <a href="#">Lyndee Lyndsey</a></h5><span>Status: </span><span class="text-success"><strong>In Stock</strong></span></div></div></td><td class="col-sm-1 col-md-1" style="text-align: center"><input type="number" class="form-control" id="quantity" value="'+itemObj.quantity+'"></td><td class="col-sm-1 col-md-1 text-center"><strong>'+itemObj.price+'</strong></td><td class="col-sm-1 col-md-1 text-center"><strong>'+itemObj.total+'</strong></td><td class="col-sm-1 col-md-1"><button type="button" class="btn btn-danger" onclick="removeCartItem(\''+itemObj.id+'\')"><span class="glyphicon glyphicon-remove"></span> Remove</button></td></tr>';
     // $('#cartList').append('<li> Item: ',itemObj.name,' Quantity: ', itemObj.quantity,'</li>');    
     $('#cartList').prepend(cartItemString);
   }
+  var total = subtotal+shipping;
+
+  $('#cartSubtotal')[0].textContent = "$ "+subtotal.toFixed(2);
+  $('#cartTotal')[0].textContent = "$ "+total.toFixed(2);
+
 }
 
 //Updated Navbar Items
