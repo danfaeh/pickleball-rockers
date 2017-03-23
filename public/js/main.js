799245a797de4446f99c90d1987907b7f27883dd
danfaeh <danfaeh@gmail.com>
2017-03-22 22:53:06 -0700

    cart calculation improvements

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,15 @@ $('#itemAddText').hide();
 //                    Cart Updates                       //
 //*******************************************************//
 
+updateNavItems();
+
 function test123(){
   alert(">>>>Paypal Checkout Integration Goes Here<<<<");
 }
 
 function removeCartItem(item){
-  console.log("removing item:", item);
   localStorage.removeItem(item);
+  updateNavItems();
   window.location.reload(false); 
 }
 
@@ -24,7 +26,18 @@ function addToCart(id,name,price){
   var cartItem = { 'id': id, 'name':name, 'quantity': itemQuantity, 'price':price, 'total':total};
   // Put the object into storage
   localStorage.setItem(id, JSON.stringify(cartItem));
+  updateNavItems();
 }   
+
+function updateNavItems(){
+  var cartQuantity = 0;
+  for(var lineItem in window.localStorage){
+    var lineItemQuantity = parseInt(JSON.parse(window.localStorage.getItem(lineItem)).quantity);
+    cartQuantity = cartQuantity + lineItemQuantity;
+  }
+  //Updated cartQuantity in Nav
+  $('#navCartItems')[0].textContent = " "+ cartQuantity + " Items";
+}
 
 //*******************************************************//
 //*******************************************************//
@@ -61,26 +74,27 @@ $(function(){
 
 // Populate Shopping Cart Items and Totals using Local Storage
 if(window.location.href.indexOf("cart") > -1){
-  var cartItems = window.localStorage;
-  var subtotal = 0;
-  var shipping = 10;
-  for (var item in cartItems){
-    itemObj = JSON.parse(window.localStorage.getItem(item));
-    subtotal = subtotal + (itemObj.price * itemObj.quantity);
-    cartItemString = '<tr><td class="col-sm-8 col-md-6"><div class="media"><a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a><div class="media-body"><h4 class="media-heading"><a href="#">'+itemObj.name+'</a></h4><h5 class="media-heading"> by <a href="#">Lyndee Lyndsey</a></h5><span>Status: </span><span class="text-success"><strong>In Stock</strong></span></div></div></td><td class="col-sm-1 col-md-1" style="text-align: center"><input type="number" class="form-control" id="quantity" value="'+itemObj.quantity+'"></td><td class="col-sm-1 col-md-1 text-center"><strong>'+itemObj.price+'</strong></td><td class="col-sm-1 col-md-1 text-center"><strong>'+itemObj.total+'</strong></td><td class="col-sm-1 col-md-1"><button type="button" class="btn btn-danger" onclick="removeCartItem(\''+itemObj.id+'\')"><span class="glyphicon glyphicon-remove"></span> Remove</button></td></tr>';
-    // $('#cartList').append('<li> Item: ',itemObj.name,' Quantity: ', itemObj.quantity,'</li>');    
-    $('#cartList').prepend(cartItemString);
+  if(window.localStorage.length===0){
+    $('#emptyCart').show();    
+    $('#cartTable').hide();
+  }else{
+    $('#emptyCart').hide();    
+    $('#cartTable').show();
+    var subtotal = 0;
+    var shipping = 10;
+    for (var item in window.localStorage){
+      itemObj = JSON.parse(window.localStorage.getItem(item));
+      subtotal = subtotal + (itemObj.price * itemObj.quantity);
+      cartItemString = '<tr><td class="col-sm-8 col-md-6"><div class="media"><a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a><div class="media-body"><h4 class="media-heading"><a href="#">'+itemObj.name+'</a></h4><h5 class="media-heading"> by <a href="#">Lyndee Lyndsey</a></h5><span>Status: </span><span class="text-success"><strong>In Stock</strong></span></div></div></td><td class="col-sm-1 col-md-1" style="text-align: center"><input type="number" class="form-control" id="quantity" value="'+itemObj.quantity+'"></td><td class="col-sm-1 col-md-1 text-center"><strong>'+itemObj.price+'</strong></td><td class="col-sm-1 col-md-1 text-center"><strong>'+itemObj.total+'</strong></td><td class="col-sm-1 col-md-1"><button type="button" class="btn btn-danger" onclick="removeCartItem(\''+itemObj.id+'\')"><span class="glyphicon glyphicon-remove"></span> Remove</button></td></tr>';
+      // $('#cartList').append('<li> Item: ',itemObj.name,' Quantity: ', itemObj.quantity,'</li>');    
+      $('#cartList').prepend(cartItemString);
+    }
+    var total = subtotal+shipping;
+
+    $('#cartSubtotal')[0].textContent = "$ "+subtotal.toFixed(2);
+    $('#cartTotal')[0].textContent = "$ "+total.toFixed(2);
   }
-  var total = subtotal+shipping;
-
-  $('#cartSubtotal')[0].textContent = "$ "+subtotal.toFixed(2);
-  $('#cartTotal')[0].textContent = "$ "+total.toFixed(2);
-
 }
-
-//Updated Navbar Items
-$('#navCartItems')[0].textContent = " "+ window.localStorage.length + " Items";
-
 
 });
 
