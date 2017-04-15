8647defe0e3e011808dd5eb7ed25e8c5433e19f8
danfaeh <danfaeh@gmail.com>
2017-04-15 10:26:38 -0700

    fixed price to have 2 decimals using getters and setters and $ in html

--- models/product.js
+++ models/product.js
@@ -10,6 +10,38 @@ var ProductSchema = mongoose.Schema({
   imageURL: String
 });
 
+// Getter
+ProductSchema.path('price').get(function(num) {
+  return (num / 100).toFixed(2);
+});
+
+// Setter
+ProductSchema.path('price').set(function(num) {
+  return num * 100;
+});
+
 var Product = mongoose.model('Product', ProductSchema);
 
 module.exports = Product;
+
+
+
+
+// var mongoose = require('mongoose');
+
+// require('mongoose-currency').loadType(mongoose);
+// var Currency = mongoose.Types.Currency;
+
+// var ProductSchema = mongoose.Schema({
+//   id: String,
+//   name: String,
+//   description: String,
+//   price: { type: Currency },
+//   available: Boolean,
+//   category: String,
+//   imageURL: String
+// });
+
+// var Product = mongoose.model('Product', ProductSchema);
+
+// module.exports = Product;
